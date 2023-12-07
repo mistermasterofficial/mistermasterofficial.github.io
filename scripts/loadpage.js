67383a83f2658dafdcceacfcd1214ee3b6a3b746
MrMaster <106398602+mistermasterofficial@users.noreply.github.com>
2023-12-07 23:57:27 +0700

    footer update

--- scripts/loadpage.js
+++ scripts/loadpage.js
@@ -19,7 +19,21 @@ document.getElementsByTagName("nav")[0].innerHTML = `
 </a>
 `;
 document.getElementsByTagName("footer")[0].innerHTML = `
-<h2><a href="https://t.me/MisterMasterOfficial">Telegram</a></h2><h2>Fonts: Caviar Dreams.<br>© Mister Master ${new Date().getFullYear()}</h2>
+<h2>
+<a href="https://t.me/MisterMasterOfficial">Telegram</a>
+</h2>
+<h2>
+<a href="https://mistermastergames.itch.io/">itch.io</a>
+</h2>
+<h2>
+<a href="https://www.behance.net/misteriomaster">Behance</a>
+</h2>
+<h2>
+<a href="https://ru.pinterest.com/misteriomaster00/">Pinterest</a>
+</h2>
+<h2>
+Fonts: Caviar Dreams.<br>© Mister Master ${new Date().getFullYear()}
+</h2>
 `;
 document.getElementsByTagName("body")[0].innerHTML = `
 <div id="top"></div>
